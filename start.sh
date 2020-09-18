060d37689fa6c1042b0bc8c8523f3e872ad89fe3
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-18 13:00:11 +0300

    final commit

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
-export MOVIE_API_KEY ='94ca7c7a0ac77f218a5d4ae9a1863817'
-export SECRET_KEY ='Flask WTF Secret Key'
+export MOVIE_API_KEY='94ca7c7a0ac77f218a5d4ae9a1863817'
+export SECRET_KEY='Flask WTF Secret Key'
+
+export MAIL_USERNAME='gakuoebenezer@gmail.com'
+export MAIL_PASSWORD='@ebay6100'
 
 
 python3.8 manage.py server
